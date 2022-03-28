ee62164af1223f94f2101bd070d15f5f6133ef71
User <User@Jill-PC>
2022-03-29 05:25:15 +0800

    java

--- src/day00_test/Test10.java
+++ src/day00_test/Test10.java
@@ -6,11 +6,11 @@ public class Test10 {
 		int age = 10;
 		int var = age++ - --age;
 		int var2= age++ - ++age;
-		int var3= --age - ++age;
+		int var3= --age - age++;
 		age= age++;
 		System.out.println(var);	//result= 0
 		System.out.println(var2);	//result= -2
-		System.out.println(var3);	//result= -1
+		System.out.println(var3);	//result= 0
 		System.out.println(age);	//result= 12
 	}
 
